839a07970fe26dbff3f3dbcc28bcc2fd86a24145
sseom <ysum1011@gmail.com>
2020-10-30 17:15:16 +0900

    [라우팅] Vue를 이용해 라우팅 기능 만들기

--- vue-trello/src/main.js
+++ vue-trello/src/main.js
@@ -1,7 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 1. 라우트 컴포넌트 : 로그인 
+const Login = {
+  template: '<div>Login Page</div>',
+}
+
+// 2. 라우트를 정의
+// 라우팅 맵을 자바스크립느 객체로 만듬
+const routes = {
+  '/': App,
+  '/login': Login,
+
+}
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  computed: {
+    VueComponent(){
+      // 경로에다라 컴포넌트를 다르게 반환예정
+      return routes[window.location.pathname] || { template: '<div>Page Not Found</div>' }
+    }
+  },
+  // render: h => h(App)
+  render(h){
+    return h(this.VueComponent)
+  }
 })
