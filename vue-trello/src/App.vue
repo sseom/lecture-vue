71b5ab2e0cc18fc17bf6334d93ccb14dda0382aa
sseom <ysum1011@gmail.com>
2020-11-03 15:48:16 +0900

    [라우팅] router-link

--- vue-trello/src/App.vue
+++ vue-trello/src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    <!-- Header : 항상 고정될 부분들 -->
-    {{ msg }}
+    <NavBar />
     <router-view></router-view>
-    <!-- Footer : 항상 고정될 부분들 -->
   </div>
 </template>
 
 <script>
+import NavBar from './components/NavBar'
+
 export default {
   name: 'app',
+  components: {
+    NavBar,
+  },
   data () {
     return {
-      msg: '라우팅 경로에 따라 화면이 바껴야하는 부분들은 router-view를 통해 컴포넌트가 대채되어 화면이 변경됨.'
+      
     }
   }
 }
